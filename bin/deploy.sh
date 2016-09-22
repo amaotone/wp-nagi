6cd6589abda7f9ed59e0887121d59971affbb054
Amane <amane.suzu@gmail.com>
2016-09-23 00:42:03 +0900

    Set build timing only for master branch

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
+    echo "Not deploying pull requests."
+    exit
+fi
+
+if [[ "master" != "$TRAVIS_BRANCH" ]]; then
+    echo "Not on the 'master' branch."
+    exit
+fi
+
 git clone -b dist --quiet "https://github.com/${TRAVIS_REPO_SLUG}.git" dist
 npm run dist
 cd dist
