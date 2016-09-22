a65fcfbb5c7fb17669736b87be8de5b216e77f75
Amane <amane.suzu@gmail.com>
2016-09-23 01:11:42 +0900

    Remove hyphen from bottom of post

--- single.php
+++ single.php
@@ -13,11 +13,11 @@
 			$eyecatchset = get_post_meta( $postID, 'eyecatch_set', true );
 		}else{
 			$eyecatchset = '';
-		} 
+		}
 		if ( has_post_thumbnail() && (( isset($GLOBALS['stdata53']) && $GLOBALS['stdata53'] === 'yes' ) || ( isset( $eyecatchset ) && $eyecatchset === 'yes' ))) { ?>
 			<div class="st-eyecatch"><?php the_post_thumbnail('full'); ?>
 			</div>
-				
+
 		<?php } //アイキャッチ画像を挿入ここまで ?>
 
 	<!--ぱんくず -->
@@ -49,7 +49,7 @@
 					<!--ループ開始 -->
 					<?php if (have_posts()) : while (have_posts()) :
 					the_post(); ?>
-					
+
 					<?php //カテゴリ表示
 					if ( isset($GLOBALS['stdata60']) && $GLOBALS['stdata60'] === 'yes' ) {
 
@@ -62,8 +62,8 @@
 					<?php
 							if ( $categories ) {
 								foreach( $categories as $category ) {
-									$output .= '<a href="' . get_category_link( $category->term_id ) . '" title="' 
-									. esc_attr( sprintf( "View all posts in %s", $category->name ) ) 
+									$output .= '<a href="' . get_category_link( $category->term_id ) . '" title="'
+									. esc_attr( sprintf( "View all posts in %s", $category->name ) )
 									. '"><span class="catname st-catid' . $category->cat_ID . '">' . $category->cat_name . '</span></a>' . $separator;
 									}
 								echo trim( $output, $separator );
@@ -72,7 +72,7 @@
 					<?php
 					} //カテゴリ表示ここまで
 					?>
-					
+
 
 					<h1 class="entry-title"><?php the_title(); //タイトル ?></h1>
 
@@ -106,12 +106,12 @@
 						<?php } ?>
 
 						<p class="tagst">
-							<i class="fa fa-folder-open-o" aria-hidden="true"></i>-<?php the_category( ', ' ) ?><br/>
-							<?php the_tags( '<i class="fa fa-tags"></i>-', ', ' ); ?>
+							<i class="fa fa-folder-open-o" aria-hidden="true"></i>&nbsp;<?php the_category( ', ' ) ?><br/>
+							<?php the_tags( '<i class="fa fa-tags"></i>&nbsp;', ', ' ); ?>
 						</p>
 
 					</div><!-- .mainboxここまで -->
-	
+
 						<?php get_template_part( 'sns' ); //ソーシャルボタン読み込み ?>
 						<?php get_template_part( 'popular-thumbnail' ); //任意のエントリ ?>
 
